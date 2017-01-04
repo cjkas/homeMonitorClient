263a6a92e67dbf38ddd883109a1cc47293db6b0d
Sławomir Czaja <slaw.cz@gmail.com>
2017-01-04 18:19:06 +0100

    last probe view auto refresh

--- app/weatherView/weatherView.js
+++ app/weatherView/weatherView.js
@@ -1,23 +1,36 @@
 'use strict';
 
 angular.module('myApp.weatherView')
-.controller('WeatherController', ['$scope', 'WeatherService', function($scope, WeatherService) {
+.controller('WeatherController', ['$scope', 'WeatherService', '$interval', function($scope, WeatherService, $interval) {
     var self = this;
-
+    var interval;
     self.probe;
-
+    self.remaining;
     fetchLast();
+
+    var check = function(){
+        var elapsed = new Date() - self.probe.created;
+        self.remaining = parseInt(600 - elapsed / 1000);
+        if(self.remaining <= 0){
+            $interval.cancel(interval);
+            fetchLast();
+        }
+    }
 
     function fetchLast(){
         WeatherService.fetchLast()
             .then(
             function(d) {
                 self.probe = d;
+                interval = $interval(check, 1000);
             },
             function(errResponse){
                 console.error('Error while fetching Contacts');
             }
         );
     }
+    $scope.$on('$destroy', function(){
+        $interval.cancel(interval);
+    });
 
 }]);
