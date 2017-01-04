02e4ab6c4833aeaa264992ad41413afabccfc1c5
Sławomir Czaja <slaw.cz@gmail.com>
2017-01-04 18:18:42 +0100

    login fix

--- app/loginView/loginView.js
+++ app/loginView/loginView.js
@@ -3,10 +3,8 @@
 angular.module('myApp.loginView').controller('LoginController', ['$scope', 'AuthSharedService', '$rootScope', function($scope, AuthSharedService, $rootScope) {
     var self = this;
     self.user = {username : "", password: ""};
-    self.userRegister = {login : "", password: ""};
 
     self.login = login;
-    self.register = register;
 
     function login(){
         $rootScope.authenticationError = false;
